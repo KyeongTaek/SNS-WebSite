7b6b0cdb4f26c20fa2c0a8cc536bb757c957dd9c
Lucas0105 <dnjswo105@gmail.com>
2022-01-30 00:06:07 +0900

    add nav component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import { NavLink } from 'react-router-dom'
+import '../style/frame.css';
 
 function Navigation() {
 
@@ -6,6 +7,52 @@ function Navigation() {
       <div className="Navigation">
         
           <nav>
+          
+            <div className="row">
+                <div className="col-lg-1 logo">
+                    <p>logo</p>
+                </div>
+                <div className="col-lg-3">
+                </div>
+
+                <div className="col-lg-4 search">
+                    <div className='searchFrame'>
+                        <input type="text" id="searchContent" name = "searchContent" placeholder='search...'/>
+                        <button className="img-button"><i className="fas fa-search fa-sm"></i></button>
+                    </div>
+
+                </div>
+
+                <div className="col-lg-4 navRight">
+                    <div className='icon'>
+                        <i className="far fa-bell fa-2x"></i>
+                        
+                        <i className="fas fa-film fa-2x"></i>
+
+                        <i className="fas fa-file-medical fa-2x"></i>
+
+                        <i className="far fa-comment-dots fa-2x"></i>
+                    </div>
+                
+                    <div className='user'>
+                        <div className='circleBox'>
+                            <i className="fas fa-user-alt"></i>
+                            
+                        </div>
+                        <div className='userStatus'>
+                            <p>Need login</p>
+                            <i className="fas fa-angle-down fa-lg"></i>
+                        </div>
+                    </div>
+                    <div className='user2Frame' id='user2Frame'>
+                        <i className="fas fa-user-alt"></i>
+
+                    </div>
+                </div>
+            </div>
+ 
+
+              <br/>
             <ul>
              
               <li>
