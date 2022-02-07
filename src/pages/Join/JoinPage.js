4cb9286b8eba1e4411e17315e3f172547b0d6983
KyeongTaek <tommyholic@naver.com>
2022-02-07 18:38:48 +0900

    Update join page

--- src/pages/Join/JoinPage.js
+++ src/pages/Join/JoinPage.js
@@ -1,7 +1,47 @@
+import '../Join/JoinPage.css';
+
 function JoinPage() {
     return (
       <div className="JoinPage">
-          <h1>JoinPage입니다.</h1>
+        <div className="row">
+          <div className="col-lg-4">
+
+          </div>
+
+          <div className="col-lg-4 joinFrame">
+            <h1>회원가입</h1>
+            <form name="fo" className="needs-validation" novalidate>
+              
+              <h2 className="identification">Full Name</h2>
+              <input type="text" placeholder="Enter Name. . ." required></input><br></br>
+              
+              <h2 className="identification">Email</h2>
+              <input type="text" placeholder="Enter Email. . ." required></input><br></br>
+              
+              <h2 className="identification">Password</h2>
+              <input type="password" placeholder="Enter password. . ." required></input><br></br>
+              <div class="valid-feedback">Valid.</div>
+              <div class="invalid-feedback">Please fill out this field.</div>
+              
+              <h2 className="identification">Confirm Password</h2>
+              <input type="password" placeholder="Enter password. . ." required></input><br></br>
+              <div class="valid-feedback">Valid.</div>
+              <div class="invalid-feedback">Please fill out this field.</div>
+              
+              <input type="submit" value="Log in"></input>
+              <ul>
+                <li><a className="joinpage" href="../idSearch">아이디 찾기</a></li>
+                <li>|</li>
+                <li><a className="joinpage" href="../pwSearch">비밀번호 찾기</a></li>
+              </ul>
+            </form>
+          </div>
+
+          <div className="col-lg-4">
+
+          </div>
+        </div>
+          
   
       </div>
     );
