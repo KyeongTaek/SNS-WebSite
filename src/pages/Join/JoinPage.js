1c7959e48b45eed880a5cab998ee3498ad25c3e7
KyeongTaek <85470519+KyeongTaek@users.noreply.github.com>
2022-02-11 17:17:48 +0900

    Loginpage, Joinpage, IdSearchpage, PwSearchpage (#39)
    * update idSearch page
    
    * update Join page
    
    * update Login page
    
    * update PwSearch page

--- src/pages/Join/JoinPage.js
+++ src/pages/Join/JoinPage.js
@@ -2,47 +2,45 @@ import '../Join/JoinPage.scss';
 
 function JoinPage() {
     return (
-      <div className="JoinPage" id='wrapper'>
-        <div className="row">
-          <div className="col-lg-4">
+      <div className="JoinPage">
+        <div className="joinFrame">
+          <div className="side">
 
           </div>
 
-          <div className="col-lg-4 joinFrame">
-            <h1>회원가입</h1>
-            <form name="fo" className="needs-validation" noValidate>
+          <form name="fo">
+            <h1>회원가입</h1>  
+            <div className="label">Full Name</div>
+            <input type="text" placeholder="Enter Name. . ." required></input><br></br>
+             
+            <div className="label">Email</div>
+            <input type="text" placeholder="Enter Email. . ." required></input><br></br>
+            
               
-              <h2 className="identification">Full Name</h2>
-              <input type="text" placeholder="Enter Name. . ." required></input><br></br>
+            <div className="label">Password</div>
+            <input type="password" placeholder="Enter password. . ." required></input><br></br>
+            <div className="valid-feedback">Valid.</div>
+            <div className="invalid-feedback">Please fill out this field.</div>
               
-              <h2 className="identification">Email</h2>
-              <input type="text" placeholder="Enter Email. . ." required></input><br></br>
-              
-              <h2 className="identification">Password</h2>
-              <input type="password" placeholder="Enter password. . ." required></input><br></br>
-              <div className="valid-feedback">Valid.</div>
-              <div className="invalid-feedback">Please fill out this field.</div>
-              
-              <h2 className="identification">Confirm Password</h2>
-              <input type="password" placeholder="Enter password. . ." required></input><br></br>
-              <div className="valid-feedback">Valid.</div>
-              <div className="invalid-feedback">Please fill out this field.</div>
-              
-              <input type="submit" value="Log in"></input>
-              <ul>
-                <li><a className="joinpage" href="../idSearch">아이디 찾기</a></li>
-                <li>|</li>
-                <li><a className="joinpage" href="../pwSearch">비밀번호 찾기</a></li>
-              </ul>
-            </form>
-          </div>
+            <div className="label">Confirm Password</div>
+            <input type="password" placeholder="Enter password. . ." required></input><br></br>
+            <div className="valid-feedback">Valid.</div>
+            <div className="invalid-feedback">Please fill out this field.</div>
 
-          <div className="col-lg-4">
+            <div className="policy">By creating an account you agree to our Terms of Service and Privacy Policy</div>
+              
+            <input type="submit" value="CONTINUE"></input>
+            <ul>
+              <li><a className="joinpage" href="../idSearch">아이디 찾기</a></li>
+              <li>|</li>
+              <li><a className="joinpage" href="../pwSearch">비밀번호 찾기</a></li>
+            </ul>
+          </form>
+          
+          <div className="side">
 
           </div>
         </div>
-          
-  
       </div>
     );
   }
