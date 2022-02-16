3c6ce4e18c1548842544d88cf0988fbe4b717416
KyeongTaek <tommyholic@naver.com>
2022-02-17 02:12:16 +0900

    Update IdSearch page

--- src/pages/IdSearch/IdSearchPage.js
+++ src/pages/IdSearch/IdSearchPage.js
@@ -2,36 +2,34 @@ import '../IdSearch/IdSearchPage.scss'
 function IdSearchPage() {
     return (
       <div className="IdSearchPage">
-          <div class="bigContainer">
-              <div class="smallContainer1">
+          <div className="bigContainer">
+              <div className="smallContainer1">
                 
               </div>
 
               <form name="IdSearch">
-                <div class="items">
+                <div className="items">
                   <h1>아이디 찾기</h1>
                   <br></br>
                   <br></br>
                 </div>
-                <div class="items">
-                  <h2 className="identification">Full Name</h2><br></br>
+                <div className="items">
+                  <div className="label">Full Name</div><br></br>
                   <input type="text" placeholder="이름 또는 닉네임을 입력해주세요" required></input><br></br>
                 </div>
-                <div class="items">
-                  <h2 className="identification">Email</h2><br></br>
-                  <input type="email" placeholder="이메일을 입력해주세요" required></input><br></br>
+                <div className="items">
+                  <div className="label">Email</div><br></br>
+                  <input type="text" placeholder="이메일을 입력해주세요" required></input><br></br>
                 </div>
-                <div class="items">
+                <div className="items">
                   <input type="submit" value="다음 단계"></input>
                 </div>
               </form>
 
-            <div class="smallContainer1">
+            <div className="smallContainer1">
               
             </div>
           </div>
-
-  
       </div>
     );
   }
