4dd421b4b98cedeac29e6871f6ab8ccc72fa3b9d
KyeongTaek <tommyholic@naver.com>
2022-02-17 02:13:22 +0900

    Update PwSearch page

--- src/pages/PwSearch/PwSearchPage.js
+++ src/pages/PwSearch/PwSearchPage.js
@@ -2,30 +2,30 @@ import '../PwSearch/PwSearchPage.scss'
 function PwSearchPage() {
     return (
       <div className="PwSearchPage">
-        <div class="bigContainer">
-          <div class="smallContainer1">
+        <div className="bigContainer">
+          <div className="smallContainer1">
 
           </div>
 
           <form name="PwSearch">
-            <div class="items">
+            <div className="items">
               <h1>비밀번호 찾기</h1>
               <br></br>
               <br></br>
             </div>
-            <div class="items">
-              <h2 className="identification">ID</h2>
+            <div className="items">
+              <div className="label">ID</div>
               <input type="text" placeholder="아이디를 입력해주세요" required></input><br></br>
             </div>
-            <div class="items">
-              <h2 className="identification">Email</h2>
-              <input type="email" placeholder="이메일을 입력해주세요" required></input><br></br>
+            <div className="items">
+              <div className="label">Email</div>
+              <input type="text" placeholder="이메일을 입력해주세요" required></input><br></br>
             </div>
-            <div class="items">
+            <div className="items">
               <input type="submit" value="다음 단계"></input>
             </div>
           </form>
-          <div class="smallContainer1">
+          <div className="smallContainer1">
             
           </div>
         </div>
