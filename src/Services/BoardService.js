f4ffc651438b17da8a2eaec75a7809dcd1a3ddea
Lucas0105 <dnjswo105@gmail.com>
2022-02-21 14:35:56 +0900

    add board register

---
+++ src/Services/BoardService.js
@@ -0,0 +1,40 @@
+import { API_BASE_URL } from "./app-config"
+
+export function call(api, method, request){
+    let headers = new Headers({
+        "Content-Type" : "application/json",
+    });
+
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if(accessToken && accessToken !==null){
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+
+    let options = {
+        headers : headers,
+        url : API_BASE_URL + api,
+        method : method,
+    };
+
+    if(request){
+        options.body = JSON.stringify(request);
+    }
+
+    return fetch(options.url, options)
+    .then((response)=>
+    response.json().then((json)=>{
+        if(!response.ok){
+            Promise.reject(json);
+        }
+        return json;
+    }))
+    .catch((error) => {
+        console.log(error.status);
+        if(error.status ===403){
+            window.location.href = "/";
+        }
+        return Promise.reject(error);
+    }
+    );
+
+}
