b7670c27ee29812fd67fee81ae052325a291bc69
KyeongTaek <tommyholic@naver.com>
2022-02-22 12:08:32 +0900

    update Report, BoardManagement

--- src/pages/Report/ReportPage.js
+++ src/pages/Report/ReportPage.js
@@ -1,33 +1,53 @@
 import { Form } from "react-bootstrap"
+import {useState} from 'react'
+import {submitReport} from '../../Services/EtcService'
+//import {utcDay} from 'd3-time'
 
 import "./ReportPage.scss";
 
 
 function ReportPage() {
+  const [report_title, setReport_title] = useState("");
+  const [report_content, setReport_content] = useState("");
+  const [report_reason, setReport_reason] = useState(0);
+  const [regulated, setRegulated] = useState("");
+
+  const doReport = (event) => {
+    event.preventDefault();
+    submitReport({report_reason:report_reason, report_title:report_title, report_content:report_content});
+  }
+
+  // const getRadio = (event) => {
+  //   event.preventDefault();
+  //   this.setReport_reason=event.target.value;
+  // }
+
+
+
     return (
       <div className="ReportPage">
         <div className="container">
           <div className="title">신고</div>
           <div className="content">
-              <Form>
+              <Form onSubmit={doReport}>
                 <span>분류</span>
                 <label for="chk1">
-                  <input type="radio" name="category" value="1" id="chk1" />욕설/비하
+                  <input type="radio" name="category" value="1" id="chk1" onclick={(event)=>{setReport_reason(event.target.value)}} />욕설/비하
                 </label>
                 <label for="chk2">
-                  <input type="radio" name="category" value="2" id="chk2" />음란물
+                  <input type="radio" name="category" value="2" id="chk2" onclick={(event)=>{setReport_reason(event.target.value)}}/>음란물
                 </label>
                 <label for="chk3">
-                  <input type="radio" name="category" value="3" id="chk3" />잘못된 정보
+                  <input type="radio" name="category" value="3" id="chk3" onclick={(event)=>{setReport_reason(event.target.value)}}/>잘못된 정보
                 </label>
                 <label for="chk4">
-                  <input type="radio" name="category" value="4" id="chk4" />자살/자해
+                  <input type="radio" name="category" value="4" id="chk4" onclick={(event)=>{setReport_reason(event.target.value)}}/>자살/자해
                 </label>
                 <label for="chk5">
-                  <input type="radio" name="category" value="5" id="chk5" />기타
+                  <input type="radio" name="category" value="5" id="chk5" onclick={(event)=>{setReport_reason(event.target.value)}}/>기타
                 </label>
-                <input type="text" class="form-control" placeholder="제목을 입력하세요" />
-                <textarea type="text" class="form-control" placeholder="상세한 신고 내용을 입력하세요" rows="10"/>
+                <input type="text" class="form-control" placeholder="제목을 입력하세요" onChange={(event)=>{setReport_title(event.target.value)}}/>
+                <textarea type="text" class="form-control" placeholder="상세한 신고 내용을 입력하세요" rows="10" onChange={(event)=>{setReport_content(event.target.value)}}/>
                 <input type="submit"/>
               </Form>
             </div>
